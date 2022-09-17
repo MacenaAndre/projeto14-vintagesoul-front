16fc28d64e9f97f681c5dd65d9b043a655dd2a3c
MacenaAndre <andremacenacosta@gmail.com>
2022-09-17 19:01:43 -0300

    Fixing Bugs & home/page

--- src/service/VintageSoulService.js
+++ src/service/VintageSoulService.js
@@ -1,6 +1,19 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000";
+const BASE_URL = "https://vintage-soul-store.herokuapp.com";
+
+function getToken() {
+	const auth = JSON.parse(localStorage.getItem("VintageSoul"));
+	if (auth) {
+		const token = {
+			headers: {
+				Authorization: `Bearer ${auth.token}`,
+			},
+		};
+		return {token, name: auth.name};
+	}
+	return false;
+}
 
 function SignInApi(body) {
     const promise = axios.post(`${BASE_URL}/sign-in`, body);
@@ -12,5 +25,16 @@ function SignUpApi(body) {
     return promise;
 };
 
+function getProductsApi(limit) {
+    const promise = axios.get(`${BASE_URL}/products?limit=${limit}`);
+    return promise;
+};
 
-export {SignInApi, SignUpApi};
+function getCart() {
+	const auth = getToken();	
+		const promise = axios.get(`${BASE_URL}/cart`,auth.token );
+		return promise;
+
+};
+
+export {SignInApi, SignUpApi, getToken, getCart, getProductsApi};
